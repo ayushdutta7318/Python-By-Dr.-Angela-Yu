4811d6f2fd62e82003b0040e9735e1f39080660f
ayushdutta7318 <ayushdutta7318@gmail.com>
2026-03-17 18:56:16 -0700

    Add files via upload

---
+++ Day-1/main.py
@@ -0,0 +1,57 @@
+# Introduction:
+
+# print is a fn which prints to the console and inside parentheses comes what you wanna print.
+
+print("Hello World!")
+
+# escape char
+
+print("Hello, Ayush\nHow are you?")
+
+# str concatenation
+
+print("hello" + " I am concatenated " + "string")
+
+    # print("") indentation error
+
+# print("Hello world) syntax error
+
+# Assignment
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a '\' and the letter n")
+
+####################################################################
+# python input fn:
+
+print("Hello " + input("Please enter your name: ") + "!")
+
+
+name = input("Enter your name: ")
+
+print(f"My name is {name}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
