18f829fa01658056dc4942531f9fd83da7ee2be1
ayushdutta7318 <ayushdutta7318@gmail.com>
2026-03-18 18:32:02 -0700

    Add files via upload

--- Day-1/main.py
+++ Day-1/main.py
@@ -34,13 +34,51 @@ name = input("Enter your name: ")
 
 print(f"My name is {name}")
 
+####################################################################
+
+# Variables in Python:
+
+num = int(input("Enter a number: "))
+
+print(f"You entered number: {num}")
+
+print(len(name))
 
 
+# Assignment:
 
+username = input("Enter your username: ")
 
+length = len(username)
 
+print(f"The length if the username is {length}")
 
+# Assignment: Swap values
 
+glass1 = "milk"
+glass2 = "juice"
+
+print(f"glass1: {glass1}")
+print(f"glass2: {glass2}")
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+print(f"glass1: {glass1}")
+print(f"glass2: {glass2}")
+
+"""
+Rules to name Variables:
+
+1. no space in b/w
+2. should not start with number
+3. can contain an underscore in b/w 2 words: eg user_name
+
+"""
+
+time_until_midnight = "5"
+print("There are "+time_until_midnight+" hours until midnight")
 
 
 
